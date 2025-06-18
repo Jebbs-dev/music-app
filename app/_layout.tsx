3821306b67dc4d47957701c8c8b868c39f119166
Jebbs-dev <ojebiyifulness@gmail.com>
2025-06-18 23:09:37 +0100

    update root layout

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,24 @@
-import '../global.css';
+import { useFonts } from "expo-font";
 import { Stack } from "expo-router";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
+import "react-native-reanimated";
+import "../global.css";
 
 export default function RootLayout() {
-  return <Stack />;
+  const [loaded] = useFonts({
+    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
+  });
+
+  if (!loaded) {
+    // Async font loading only occurs in development.
+    return null;
+  }
+
+  return (
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <Stack>
+        <Stack.Screen name="(home)" options={{ title: "Home", headerShown: false }} />
+      </Stack>
+    </GestureHandlerRootView>
+  );
 }
