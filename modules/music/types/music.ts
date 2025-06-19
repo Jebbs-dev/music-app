f2f64124a4d2932e488a8692d50c482d19b6624c
Jebbs-dev <ojebiyifulness@gmail.com>
2025-06-19 12:53:22 +0100

    feat: create music type

---
+++ modules/music/types/music.ts
@@ -0,0 +1,11 @@
+export type MusicType = {
+  id: string;
+  title: string;
+  artist: string;
+  artwork: string;
+  url: string;
+  // duration?: number; // Optional duration field
+  // isFavorite?: boolean; // Optional favorite status
+  // isPlaying?: boolean; // Optional playing status
+  // isPaused?: boolean; // Optional paused status
+}
