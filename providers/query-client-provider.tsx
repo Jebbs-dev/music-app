0922c4413730a1fd7678c02969ddbd5067821cdf
Jebbs-dev <ojebiyifulness@gmail.com>
2025-07-16 21:19:23 +0100

    feat: add query client provider

---
+++ providers/query-client-provider.tsx
@@ -0,0 +1,14 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+interface ProviderProps {
+  children: React.ReactNode;
+}
+
+
+export const QueryProvider: React.FC<ProviderProps> = ({ children }) => {
+  const queryClient = new QueryClient();
+
+  return (
+    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+  );
+};
