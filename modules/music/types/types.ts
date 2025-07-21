ba2a226f53dfc62bcd8daa214f4c880cd319b4f7
Jebbs-dev <ojebiyifulness@gmail.com>
2025-07-21 21:45:52 +0100

    feat: update types for music, album and songs

--- modules/music/types/types.ts
+++ modules/music/types/types.ts
@@ -11,26 +11,40 @@ export type User = {
 export type Artist = {
   id: string;
   name: string;
+  email?: string;
+  description?: string;
+  image?: string;
+  status?: "ACTIVE" | "INACTIVE";
+  createdAt?: string;
+  updatedAt?: string;
+  albums?: Album[];
+
 }
 
 export type Album = {
   id: string;
   title: string;
+  artistId?: string;
+  coverImage: string;
+  releaseDate?: number;
+  createdAt?: string;
+  updatedAt?: string;
+  artist?: Artist;
 }
 
 
 export type SongData = {
   id: string;
   title: string;
-  artistId: string;
+  artistId?: string;
   albumId?: string;
   coverImage: string;
   url: string;
-  releaseDate: number;
+  releaseDate?: number;
   albumPosition?: number;
   duration?: number;
-  createdAt: string;
-  updatedAt: string;
-  artist?: Artist;
+  createdAt?: string;
+  updatedAt?: string;
+  artist: Artist;
   album?: Album;
 }
