653bf8ba66ee939261b432dd505ccbcfb81655bd
Jebbs-dev <ojebiyifulness@gmail.com>
2025-07-21 21:58:29 +0100

    feat: add data for homepage

--- app/(home)/index.tsx
+++ app/(home)/index.tsx
@@ -4,6 +4,7 @@ import {
   SafeAreaView,
   ScrollView,
   TouchableOpacity,
+  Image,
 } from "react-native";
 import React, { useEffect, useState } from "react";
 import { LinearGradient } from "expo-linear-gradient";
@@ -14,11 +15,17 @@ import { useMusicControls } from "@/store/music-controls";
 import { useMusicView } from "@/store/music-view";
 import PlayingMini from "@/components/PlayingMini";
 import Entypo from "@expo/vector-icons/Entypo";
-import Ionicons from '@expo/vector-icons/Ionicons';
-import FontAwesome from '@expo/vector-icons/FontAwesome';
+import Ionicons from "@expo/vector-icons/Ionicons";
+import FontAwesome from "@expo/vector-icons/FontAwesome";
+import { Link } from "expo-router";
 
 const HomePage = () => {
-  const { playerView, setPlayerView } = useMusicView();
+  const { playerView, setPlayerView, setAppModalView } = useMusicView();
+  const { data, albumsData, artistsData } = useMusicData();
+
+  const filteredAlbums = albumsData.filter(
+    (album) => album.artistId === artistsData[1]?.id
+  );
 
   const categories = [
     "Podcasts",
@@ -50,15 +57,18 @@ const HomePage = () => {
             </View>
             <View className="flex flex-row gap-6">
               <TouchableOpacity>
-                <Ionicons name="notifications-outline" size={24} color="white" />
+                <Ionicons
+                  name="notifications-outline"
+                  size={24}
+                  color="white"
+                />
               </TouchableOpacity>
-              <TouchableOpacity>
+              <Link href="/search-modal">
                 <Ionicons name="search-outline" size={24} color="white" />
-              </TouchableOpacity>
+              </Link>
               <TouchableOpacity>
                 <FontAwesome name="circle" size={24} color="gray" />
               </TouchableOpacity>
-                
             </View>
           </View>
           <View className="mt-5">
@@ -95,27 +105,63 @@ const HomePage = () => {
           <View className="mt-10 px-4 flex flex-col gap-4">
             <View className="flex flex-row justify-between items-center">
               <View className="flex flex-row items-center gap-3">
-                <View className="h-12 w-12 rounded-full bg-white"></View>
+                <View className="h-12 w-12 rounded-full bg-white">
+                  <Image
+                    source={
+                      typeof artistsData[1]?.image === "string"
+                        ? { uri: artistsData[1]?.image }
+                        : artistsData[1]?.image
+                    }
+                    alt="image"
+                    width={25}
+                    height={25}
+                    className="w-full h-full rounded-full"
+                  />
+                </View>
                 <Text className="text-2xl font-bold text-white">
-                  Artist Name
+                  {artistsData[1]?.name}
                 </Text>
               </View>
               <View>
-                <Entypo name="chevron-thin-right" size={15} color="white" />
+                <Link
+                  href="/artist-profile"
+                  // onPress={() => {
+                  //   setCurrentArtist(artistsData[1]);
+                  // }}
+                >
+                  <Entypo name="chevron-thin-right" size={15} color="white" />
+                </Link>
               </View>
             </View>
             <ScrollView horizontal showsHorizontalScrollIndicator={false}>
-              {Array(7)
-                .fill(0)
-                .map((_, idx) => (
-                  <View key={idx} className="mr-4">
-                    <View className="h-40 w-40 rounded-md bg-white/30" />
-                    <Text className="text-white text-lg font-semibold mt-2">
-                      Album Name
-                    </Text>
-                    <Text className="text-gray-400">Album . Artist</Text>
+              {filteredAlbums.map((album, idx) => (
+                <View key={idx} className="mr-4">
+                  <View className="h-40 w-40 rounded-md bg-white/30">
+                    {typeof album.coverImage === "string" ? (
+                      <Image
+                        source={
+                          typeof album.coverImage === "string"
+                            ? { uri: album.coverImage }
+                            : album.coverImage
+                        }
+                        alt="image"
+                        width={25}
+                        height={25}
+                        className="w-full h-full rounded-md"
+                      />
+                    ) : (
+                      <View className="h-40 w-40 rounded-md bg-white/30" />
+                    )}
                   </View>
-                ))}
+
+                  <Text className="text-white text-lg font-semibold mt-2">
+                    {album.title}
+                  </Text>
+                  <Text className="text-gray-400">
+                    Album . {album.artist?.name}
+                  </Text>
+                </View>
+              ))}
             </ScrollView>
           </View>
 
