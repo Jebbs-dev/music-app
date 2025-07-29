7b4d71ec2c37bb7ea990e769aabab22db26b728d
Jebbs-dev <ojebiyifulness@gmail.com>
2025-07-29 22:39:06 +0100

    feat: add global player state and state for last played song

--- store/music-controls.ts
+++ store/music-controls.ts
@@ -1,9 +1,14 @@
 import { SongData } from "@/modules/music/types/types";
+import { AudioPlayer } from "expo-audio";
 import { create } from "zustand";
 
 interface MusicControlsState {
   isPlaying: boolean;
   setIsPlaying: (playing: boolean) => void;
+  player: AudioPlayer | null; // Use AudioPlayer type from expo-audio
+  setPlayer: (player: AudioPlayer) => void;
+  lastPlayedSongUrl: string | undefined;
+  setLastPlayedSongUrl: (url: string) => void;
   sound: any;
   setSound: (sound: any) => void;
   currentSongIndex: number;
@@ -25,6 +30,10 @@ interface MusicControlsState {
 export const useMusicControls = create<MusicControlsState>((set) => ({
   isPlaying: false,
   setIsPlaying: (playing) => set({ isPlaying: playing }),
+  player: null,
+  setPlayer: (player) => set({ player }),
+  lastPlayedSongUrl: undefined,
+  setLastPlayedSongUrl: (url: string) => set({ lastPlayedSongUrl: url }),
   sound: null,
   setSound: (sound) => set({ sound }),
   currentSongIndex: 0,
