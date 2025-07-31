2b8a52f375d1eb6e412df4c97414b65ff4614a23
Jebbs-dev <ojebiyifulness@gmail.com>
2025-07-31 21:27:51 +0100

    feat: protected screens based on login status

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -4,8 +4,11 @@ import { Stack } from "expo-router";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 import "react-native-reanimated";
 import "../global.css";
+import useAuthStore from "@/store/auth-store";
 
 export default function RootLayout() {
+  const { isLoggedIn } = useAuthStore();
+
   const [loaded] = useFonts({
     SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
   });
@@ -19,26 +22,45 @@ export default function RootLayout() {
     <QueryProvider>
       <GestureHandlerRootView style={{ flex: 1 }}>
         <Stack>
-          <Stack.Screen
-            name="(home)"
-            options={{ title: "Home", headerShown: false }}
-          />
-          <Stack.Screen
-            name="search-modal"
-            options={{
-              presentation: "fullScreenModal",
-              headerShown: false,
-              animation: "fade",
-            }}
-          />
-          <Stack.Screen
-            name="artist-profile"
-            options={{
-              presentation: "fullScreenModal",
-              headerShown: false,
-              animation: "slide_from_left"
-            }}
-          />
+          <Stack.Protected guard={isLoggedIn}>
+            <Stack.Screen
+              name="(home)"
+              options={{ title: "Home", headerShown: false }}
+            />
+          </Stack.Protected>
+
+          <Stack.Protected guard={!isLoggedIn}>
+            <Stack.Screen
+              name="login"
+              options={{
+                presentation: "fullScreenModal",
+                headerShown: false,
+                animation: "fade",
+              }}
+            />
+          </Stack.Protected>
+
+          <Stack.Protected guard={isLoggedIn}>
+            <Stack.Screen
+              name="search-modal"
+              options={{
+                presentation: "fullScreenModal",
+                headerShown: false,
+                animation: "fade",
+              }}
+            />
+          </Stack.Protected>
+
+          <Stack.Protected guard={isLoggedIn}>
+            <Stack.Screen
+              name="artist-profile"
+              options={{
+                presentation: "fullScreenModal",
+                headerShown: false,
+                animation: "slide_from_left",
+              }}
+            />
+          </Stack.Protected>
         </Stack>
       </GestureHandlerRootView>
     </QueryProvider>
