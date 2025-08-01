4d90f4b4ba8e6a068d06ddcb9de2ab0b7361032f
Jebbs-dev <ojebiyifulness@gmail.com>
2025-08-01 22:40:11 +0100

    feat: add view states for modals

--- store/music-view.ts
+++ store/music-view.ts
@@ -9,16 +9,27 @@ interface MusicViewState {
   setOverlayView: (view: "player" | "options") => void;
   musicViewOption: "up next" | "lyrics" | "related";
   setMusicViewOption: (viewOption: "up next" | "lyrics" | "related") => void;
+  artistModalVisible: boolean;
+  setArtistModalVisible: (visible: boolean) => void;
+  searchModalVisible: boolean;
+  setSearchModalVisible: (visible: boolean) => void;
+  albumModalVisible: boolean; // Optional for future use
+  setAlbumModalVisible: (visible: boolean) => void; // Optional for future use
 }
 
 export const useMusicView = create<MusicViewState>((set) => ({
   dynamicColor: "#7A0C15",
-  setDynamicColor: (color) => set({dynamicColor: color}),
+  setDynamicColor: (color) => set({ dynamicColor: color }),
   playerView: "minimized",
   setPlayerView: (view) => set({ playerView: view }),
   overlayView: "player",
   setOverlayView: (view) => set({ overlayView: view }),
   musicViewOption: "up next",
-  setMusicViewOption: (viewOption) => set({ musicViewOption: viewOption  })
+  setMusicViewOption: (viewOption) => set({ musicViewOption: viewOption }),
+  artistModalVisible: false,
+  setArtistModalVisible: (visible) => set({ artistModalVisible: visible }),
+  searchModalVisible: false,
+  setSearchModalVisible: (visible) => set({ searchModalVisible: visible }),
+  albumModalVisible: false, // Optional for future use
+  setAlbumModalVisible: (visible) => set({ albumModalVisible: visible }),
 }));
-
