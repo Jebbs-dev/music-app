3c2822394a39c1c587b112672266e90ea80f2f99
Jebbs-dev <ojebiyifulness@gmail.com>
2025-08-06 21:18:13 +0100

    feat: add library data options state

--- store/library-view.ts
+++ store/library-view.ts
@@ -1,11 +1,19 @@
 import { create } from "zustand";
 
+export type LibraryOptionsTypes = "overview" | "songs" | "albums" | "artists" | "playlists" | "podcasts";
+
 interface LibraryViewState {
   libraryDataView: "listView" | "gridView";
   setLibraryDataView: (view: "listView" | "gridView") => void;
+  libraryOptions: LibraryOptionsTypes;
+  setLibraryOptions: (
+    option: LibraryOptionsTypes
+  ) => void;
 }
 
 export const useLibraryView = create<LibraryViewState>((set) => ({
   libraryDataView: "listView",
   setLibraryDataView: (view) => set({ libraryDataView: view }),
+  libraryOptions: "overview",
+  setLibraryOptions: (option) => set({ libraryOptions: option }),
 }));
